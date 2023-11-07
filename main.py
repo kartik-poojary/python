98262921ae28d4c145d3d1d1c759223da0606ae3
kartik-poojary <kartikvpoojary0805@gmail.com>
2023-11-07 11:27:23 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+"""v = 'Everyone'
+print('Hello World\n' + v )"""
+#print("You are targeting " + input("What ip would you like to target ?\n" ))
+"""print ("Enter two number")
+x =int(input())
+y=int(input())
+print(x+y)"""
+print("k")
